1171b33ea9380eedb0ec683df1dbb637a84940ad
rifqi5300 <rifqi5300@gmail.com>
2017-11-23 12:09:44 +0700

    4 commits

--- SmartTicketing/Controllers/HomeController.cs
+++ SmartTicketing/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SmartTicketing.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your web application description page.";
 
             return View();
         }
